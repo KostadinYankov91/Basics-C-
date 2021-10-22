dcd0c9b1dec0fb339faa5242d1fec234b701062d
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-10-22 17:55:05 +0300

    Add files via upload

---
+++ Skeleton/Contracts/ITarget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skeleton.Contracts
+{
+    public interface ITarget
+    {
+        void TakeAttack(int attack);
+        bool IsDead();
+
+        int GiveExperience();
+    }
+}
