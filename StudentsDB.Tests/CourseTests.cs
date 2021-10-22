9cc4985f13b3ff1eeb8b08dbb86a6623c4ab17d3
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-10-22 17:57:06 +0300

    Add files via upload

---
+++ StudentsDB.Tests/CourseTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StudentsDB.Tests
+{
+    [TestFixture]
+    public class CourseTests
+    {
+        [Test]
+        public void When_DisplayAllStudentsIsCalled_ShouldDisplayAll()
+        { 
+            Course
+        }
+    }
+}
