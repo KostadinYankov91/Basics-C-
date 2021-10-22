205dc1c98fe9d17343f777e70ae2564d24efb6b9
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-10-22 22:23:13 +0300

    Add files via upload

---
+++ ObjectsAndClassesExercises/students/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace students
+{
+    class Student
+    { 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Hometown { get; set; }
+    
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                string[] tokens = line.Split();
+
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                string age = tokens[2];
+                string homeTown = tokens[3];
+
+                Student student = new Student();
+
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.Hometown = homeTown;
+
+                students.Add(student);
+
+                line = Console.ReadLine();
+            }
+
+            string city = Console.ReadLine();
+
+            List<Student> filteredStudents = students.Where(x => x.Hometown == city).ToList();
+
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+}
---
+++ ObjectsAndClassesExercises/studentsVersion2/Program.cs
@@ -0,0 +1,109 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace students
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Hometown { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                string[] tokens = line.Split();
+
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                string age = tokens[2];
+                string homeTown = tokens[3];
+
+                Student student = new Student();
+
+                student.FirstName = firstName;
+                student.LastName = lastName;
+                student.Age = age;
+                student.Hometown = homeTown;
+
+                students.Add(student);
+
+                if (IsStudentExist(students, firstName, lastName))
+                {
+                    student = GetStudent(students, firstName, lastName, age);
+
+                    student.FirstName = firstName;
+                    student.LastName = lastName;
+                    student.Age = age;
+                    student.Hometown = homeTown;
+                }
+                else
+                {
+                    student = new Student();
+                    {
+                        student.FirstName = firstName;
+                        student.LastName = lastName;
+                        student.Age = age;
+                        student.Hometown = homeTown;
+                    }
+
+                    students.Add(student);
+                }
+
+                line = Console.ReadLine();
+            }
+
+            string city = Console.ReadLine();
+
+
+            List<Student> filteredStudents = students.Where(x => x.Hometown == city).ToList();
+
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+
+        static bool IsStudentExist(List<Student> students, string firstName, string lastName)
+        {
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static Student GetStudent(List<Student> students, string firstName, string lastName, string age)
+        {
+            Student existingStudent = null;
+
+            foreach (Student student in students)
+            {
+                if (student.FirstName == firstName && student.LastName == lastName)
+                {
+                    existingStudent = student;
+                    existingStudent.Age = age;
+                }
+            }
+
+            return existingStudent;
+        }
+    }
+}
