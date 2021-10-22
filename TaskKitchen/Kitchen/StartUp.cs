cd96f5bbabf7f3f2e8934e5c3767f4780f0606fb
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-10-22 21:55:39 +0300

    Add files via upload

---
+++ TaskKitchen/Kitchen/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                Person currPerson = new Person(input[0], input[1], int.Parse(input[2]));
+                people.Add(currPerson);
+            }
+
+            people.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
