b78062baddb5e68acce68e4a5df6807fc2623e92
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-10-22 17:59:13 +0300

    Add files via upload

---
+++ TaskFestivalManager/FestivalManager/Entities/Stage.cs
@@ -0,0 +1,60 @@
+﻿namespace FestivalManager.Entities
+{
+	using System.Collections.Generic;
+	using Contracts;
+
+	public class Stage : IStage
+	{
+		// да си вкарват през полетата бе
+		public readonly List<ISet> Sets;
+		public readonly List<ISong> Songs;
+		public readonly List<IPerformer> Performers;
+        public IReadOnlyCollection<ISet> Sets { get; }
+        public IReadOnlyCollection<ISong> Songs { get; }
+        public IReadOnlyCollection<IPerformer> Performers { get; }
+        public IPerformer GetPerformer(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISong GetSong(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ISet GetSet(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddPerformer(IPerformer performer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddSong(ISong song)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddSet(ISet performer)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasPerformer(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasSong(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasSet(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
