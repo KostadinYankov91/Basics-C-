38779e1a5be67a6c6a13bf8efebc60031c4e2158
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-10-22 22:29:51 +0300

    Add files via upload

---
+++ UnitTestingExercise/Bank.Tests/BankAccountTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Bank.Tests
+{
+    [TestFixture]
+    public class BankAccountTests
+    {
+        private decimal amount;
+        private string name;
+        private BankAccount bankAccount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.name = "Pesho";
+            this.amount = 5;
+            this.bankAccount = new BankAccount(this.amount, this.name);
+        }
+
+        [Test]
+        public void When_AccountInitializedWithPositiveValue_AmountShould_BeChanged()
+        {
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount) /*"Six should not be equal to five"*/);
+            Assert.AreEqual(bankAccount.Amount, amount);
+        }
+
+        [Test]
+        public void When_AccountDepositIsMade_ShouldChangeAmount()
+        {
+            bankAccount.Deposit(amount);
+
+            Assert.That(bankAccount.Amount, Is.EqualTo(amount * 2));
+        }
+    }
+}
+  
