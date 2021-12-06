fbc62c547f587760d3390ea0ee52f233d2b5dfe1
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-12-06 22:33:58 +0200

    Add files via upload

---
+++ EntityFrameworkCoreMusicHub Database_Skeleton/MusicHub/Common/ValidationConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicHub.Common
+{
+    public class ValidationConstants
+    {
+
+        public const int SongMaxLength = 20;
+        public const int NameMaxLength = 40;
+        public const int PerformerMaxLength = 20;
+        public const int ProducerMaxLength = 30;
+        public const int WriterMaxLength = 20;
+    }
+}
