8c0d4f94a0502453d6ee5acc5ed9becbbbba555e
Kostadin Yankov <72874779+KostadinYankov91@users.noreply.github.com>
2021-12-06 22:33:15 +0200

    Add files via upload

---
+++ EntityFrameworkCoreJSON-Processing-Product-Shop-Skeleton/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ProductShop.Dtos;
+using ProductShop.Dtos.Inputs;
+using ProductShop.Dtos.Output;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputDto, User>();
+            CreateMap<ProductInputDto, Product>();
+            CreateMap<CategoryInputDto, Category>();
+            CreateMap<CategoryProductsInputDto, CategoryProduct>();
+            CreateMap<Product, ProductOutputDto>()
+                .ForMember(dest => dest.Seller, opt => opt.MapFrom(src => $"{src.Seller.FirstName} {src.Seller.LastName}" ));
+        }
+    }
+}
---
+++ EntityFrameworkCoreJSON-Processing-Product-Shop-Skeleton/ProductShop/StartUp.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using ProductShop.Data;
+using ProductShop.Dtos;
+using ProductShop.Dtos.Inputs;
+using ProductShop.Dtos.Output;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+
+        private static IMapper mapper;
+
+        public static void Main(string[] args)
+        {
+            var context = new ProductShopContext();
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            //var usersJSONString = File.ReadAllText("../../../Datasets/users.json");
+            //var productsJSONString = File.ReadAllText("../../../Datasets/products.json");
+            //var categoriesJSONString = File.ReadAllText("../../../Datasets/categories.json");
+            //var categoryProductsJSONString = File.ReadAllText("../../../Datasets/categories-products.json");
+
+            //Console.WriteLine(ImportUsers(context, usersJSONString));
+            //Console.WriteLine(ImportProducts(context, productsJSONString));
+            //Console.WriteLine(ImportCategories(context, categoriesJSONString));
+            //Console.WriteLine(ImportCategoryProducts(context, categoryProductsJSONString));
+
+            Console.WriteLine(GetProductsInRange(context));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<UserInputDto> users = JsonConvert.DeserializeObject<IEnumerable<UserInputDto>>(inputJson);
+
+            InitializeMapper();
+
+            IEnumerable<User> mappedUsers = mapper.Map<IEnumerable<User>>(users);
+
+            context.Users.AddRange(mappedUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedUsers.Count()}";
+        }
+
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<ProductInputDto> products = JsonConvert.DeserializeObject<IEnumerable<ProductInputDto>>(inputJson);
+
+            InitializeMapper();
+
+            var mappedProducts = mapper.Map<IEnumerable<Product>>(products);
+
+            context.Products.AddRange(mappedProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedProducts.Count()}";
+        }
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<CategoryInputDto> categories = JsonConvert.DeserializeObject<IEnumerable<CategoryInputDto>>(inputJson)
+                .Where(x => !string.IsNullOrEmpty(x.Name));
+
+            InitializeMapper();
+
+            var mappedCategories = mapper.Map<IEnumerable<Category>>(categories);
+
+            context.Categories.AddRange(mappedCategories);
+            context.SaveChanges();
+
+            return $"Successfully imported {mappedCategories.Count()}";
+        }
+
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<CategoryProductsInputDto> categoryProducts = JsonConvert.DeserializeObject<IEnumerable<CategoryProductsInputDto>>(inputJson);
+
+            InitializeMapper();
+
+            var mappedCategoryProducts = mapper.Map<IEnumerable<CategoryProduct>>(categoryProducts);
+
+            context.CategoryProducts.AddRange(mappedCategoryProducts);
+            context.SaveChanges();  
+
+            return $"Successfully imported {mappedCategoryProducts.Count()}"; 
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            InitializeMapper();
+            
+            var products = context.Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .OrderBy(x => x.Price)
+                .ProjectTo<ProductOutputDto>(mapper.ConfigurationProvider)
+                //.Select(x => new UserOutputDto
+                //{
+                //    Name = x.Name,
+                //    Price = x.Price,
+                //    Seller = $"{x.Seller.FirstName} {x.Seller.LastName}"
+                //})
+                .ToList();
+
+
+            var mappedResult = mapper.Map<IEnumerable<ProductOutputDto>>(products);
+
+
+            DefaultContractResolver contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+
+            var jsonSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = contractResolver
+            };
+
+            string productsAsJson = JsonConvert.SerializeObject(products, jsonSettings);
+
+            return productsAsJson;
+        }
+
+        private static void InitializeMapper()
+        {
+            var mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = new Mapper(mapperConfiguration);
+        }
+    }
+
+    //public static class UserMappings
+
+    //{
+    //    public static User MapToDomainUser(this UserInputDto userDto)
+    //    {
+    //        return new User
+    //        {
+    //            Age = userDto.Age,
+    //            FirstName = userDto.FirstName,
+    //            LastName = userDto.LastName
+    //        };
+    //    }
+    //}
+}
